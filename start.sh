e07de41f0ca56a13acc6a10dcece54efe4e4c654
iii <b97607065@gmail.com>
2017-08-31 23:45:10 +0800

    Delete repeated file and update some content
    that's all

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8888:8888 --name iii-tutorial -v $(pwd)/tutorial:/home/jovyan/ jupyter/base-notebook start-notebook.sh --NotebookApp.token=''
+docker run -d -p 5000:5000 -p 8888:8888 --name iii-tutorial -v $(pwd)/tutorial:/home/jovyan/ jupyter/base-notebook start-notebook.sh --NotebookApp.token=''
--- tutorial/.bash_history
+++ tutorial/.bash_history
@@ -7,3 +7,9 @@ wget localhost:5000?hello=123
 wget http://localhost:5000?hello=123
 cat index.html\?hello\=123 
 exit
+aws configure
+pip install awscli
+aws configure
+ls .aws/
+cat .aws/config 
+cat .aws/credentials 
